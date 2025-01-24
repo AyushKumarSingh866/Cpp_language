06ef1611e06c3bdabf438c8a0a8dc37d7741fd1d
AyushKumarSingh866 <ayushkumarsingh866@gmail.com>
2025-01-25 00:12:19 +0530

    Learnig C++ If_Else statement

---
+++ C++/if_else.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int marks;
+    cout<<"Enter the marks obtained: ";
+    cin>>marks;
+    if (marks>33)
+    {
+        cout<<"You are passed";
+    }
+    else
+    {
+        cout<<"You are failed";
+    }
+    
+}
