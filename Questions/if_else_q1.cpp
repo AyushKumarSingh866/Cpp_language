761db65b8c932d4061836df01d3606b70dd99928
AyushKumarSingh866 <ayushkumarsingh866@gmail.com>
2025-01-25 00:11:43 +0530

    Questions solving of If else

---
+++ Questions/if_else_q1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int number;
+    cout<<"Enter the number to check whether it is even or not: ";
+    cin>>number;
+    if (number%2==0)
+    {
+        cout<<"Even";
+    }
+    else
+    {
+        cout<<"Odd";
+    }
+    
+    
+}
---
+++ Questions/positiveNegative.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter the Number: ";
+    cin>>num;
+    if (num>0)
+    {
+        cout<<"Positive";
+    }
+    else if (num==0) 
+    {
+        cout<<"Zero";
+    }
+    else{
+        cout<<"negative";
+    }
+    
+}
