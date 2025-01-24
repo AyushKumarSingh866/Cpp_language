9e59d156383f887c3c9016fbe834bceb03de8277
AyushKumarSingh866 <ayushkumarsingh866@gmail.com>
2025-01-25 00:29:37 +0530

    Questions of If_else

---
+++ Questions/vowels_ifelse.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char character;
+    cout<<"Enter the character to check whether it is a vowel or not: ";
+    cin>>character;
+    if (character =='a')
+    {
+        cout<<"Vowel";
+    }
+    else if (character == 'e')
+    {
+        cout<<"Vowel";
+    }
+      else if (character == 'i')
+    {
+        cout<<"Vowel";
+    }
+      else if (character == 'o')
+    {
+        cout<<"Vowel";
+    }
+      else if (character == 'u')
+    {
+        cout<<"Vowel";
+    }
+    else{
+        cout<<"Consonant";
+    }
+    
+    
+}
