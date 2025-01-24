74ad93d46896540ba3062ae760b7f4f967182985
AyushKumarSingh866 <ayushkumarsingh866@gmail.com>
2025-01-25 01:30:16 +0530

    Questions solving of C++

---
+++ Questions/evenNumsupto20.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int num;
+   cout<<"Enter the number upto which you want even numbers: ";
+   cin>>num;
+    for (int i=2; i <=num; i=i+2)
+    {
+        cout<<i<<endl;
+    }
+    
+
+}
---
+++ Questions/squaresOfNumber.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number of upto which you want its squares: ";
+    cin>>n;
+    for ( int i = 1; i <=n; i++)
+    {
+        cout<<"Squares of the numbers are: "<<i*i<<endl;
+    }
+    
+}
