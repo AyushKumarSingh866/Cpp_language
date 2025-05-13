b88950be10009477262b32bdc66465222296849b
AyushKumarSingh866 <ayushkumarsingh866@gmail.com>
2025-05-13 12:20:49 +0530

    Questions of array

--- C++/Arrays/Array.cpp
+++ C++/Arrays/Array.cpp
@@ -1,15 +1,11 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-void fun (int a[], int n){
-    cout<<sizeof(a)<<endl;
-    for (int i = 0; i < n; i++)
-    {
-        cout<<a[i]<<" ";
-    }
-    
-}
 int main(){
-    int arr[5]={1,2,3,4,5,};
-    cout<<sizeof(arr)<<endl;
-    fun(arr,5);
-}
+    int i;
+    int arr[5]={5,6,9,7,6};
+    for ( i = 0; i < 5; i++)
+    {
+     cout<<arr[i];
+    }
+};
+
